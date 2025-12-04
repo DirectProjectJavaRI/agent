8ef05c1daf092fa7ef4cd9786897a0e5adb17a48
madison <madison@madisons-mbp-2.lan>
2025-12-04 08:35:12 -0600

    Enhanced Support for OAEP params and cipher names.
    Updates cipher param names for OAEP use cases.  Also updates how OAEP
    params are passed to the underlying JCE provider and creates a fallback
    to use no params (and rely only on the cipher name).

--- src/main/java/org/nhindirect/stagent/cryptography/bc/DirectNamedJcaJceExtHelper.java
+++ src/main/java/org/nhindirect/stagent/cryptography/bc/DirectNamedJcaJceExtHelper.java
@@ -5,7 +5,13 @@ import java.security.PrivateKey;
 import javax.crypto.SecretKey;
 
 import org.apache.commons.lang3.StringUtils;
+import org.bouncycastle.asn1.ASN1Encodable;
+import org.bouncycastle.asn1.DERSequence;
+import org.bouncycastle.asn1.DLSequence;
+import org.bouncycastle.asn1.nist.NISTObjectIdentifiers;
+import org.bouncycastle.asn1.oiw.OIWObjectIdentifiers;
 import org.bouncycastle.asn1.pkcs.PKCSObjectIdentifiers;
+import org.bouncycastle.asn1.pkcs.RSAESOAEPparams;
 import org.bouncycastle.asn1.x509.AlgorithmIdentifier;
 import org.bouncycastle.jcajce.util.NamedJcaJceHelper;
 import org.bouncycastle.operator.SymmetricKeyUnwrapper;
@@ -26,15 +32,71 @@ public class DirectNamedJcaJceExtHelper extends NamedJcaJceHelper implements Dir
     	retVal.setProvider(providerName);
     	
     	/*
-    	 * For a non-BC provider, we need to map the OAEP algorithm OID to a name.  Many HSMs do not recognized the algorithm OID and explicitly
-    	 * need the name.  May need to get sophisticated in later versions to map names for specific HSMs. 
+    	 * Use explicit names to indicate parameters for OAEP so we don't have to pass them later
+    	 * use a parameter structure (some providers may not support setting parameters via a parameters
+    	 * structure).
     	 */
-    	if (!StringUtils.isEmpty(providerName) && !providerName.equalsIgnoreCase("BC"))
+
+    	if (PKCSObjectIdentifiers.id_RSAES_OAEP.equals(keyEncryptionAlgorithm.getAlgorithm()))
     	{
-    		retVal.setAlgorithmMapping(PKCSObjectIdentifiers.id_RSAES_OAEP, "RSA/None/OAEPWithSHA1AndMGF1Padding");
+    		ASN1Encodable asn1Encodable = keyEncryptionAlgorithm.getParameters();
+    		
+            RSAESOAEPparams rsaesoaePparams = null;
+
+            if( asn1Encodable instanceof DERSequence) {
+                DERSequence derSequence = (DERSequence) asn1Encodable;
+                rsaesoaePparams = RSAESOAEPparams.getInstance(derSequence);
+            }
+            if( asn1Encodable instanceof DLSequence) {
+                DLSequence dlSequence = (DLSequence) asn1Encodable;
+                rsaesoaePparams = RSAESOAEPparams.getInstance(dlSequence);
+            }
+            
+        	if (!StringUtils.isEmpty(providerName) && !providerName.equalsIgnoreCase("BC"))
+        	{
+        		// special cases for different HSMs
+        		if (providerName.equalsIgnoreCase("SAFENETPROTECTWRAPPER"))
+					retVal.setAlgorithmMapping(PKCSObjectIdentifiers.id_RSAES_OAEP, "RSA/ECB/OAEP");
+				else
+				{
+					// Default use case for LUNA
+	                if( rsaesoaePparams != null) {
+	                	String algorithm = rsaesoaePparams.getHashAlgorithm().getAlgorithm().getId();
+	                	if (algorithm.equals(NISTObjectIdentifiers.id_sha256.getId()))
+	                		retVal.setAlgorithmMapping(PKCSObjectIdentifiers.id_RSAES_OAEP, "RSA/None/OAEPWithSHA-256AndMGF1Padding");
+	                	else if (algorithm.equals(NISTObjectIdentifiers.id_sha384.getId()))
+	                		retVal.setAlgorithmMapping(PKCSObjectIdentifiers.id_RSAES_OAEP, "RSA/None/OAEPWithSHA-384AndMGF1Padding");
+	                	else if (algorithm.equals(NISTObjectIdentifiers.id_sha512.getId()))
+	                		retVal.setAlgorithmMapping(PKCSObjectIdentifiers.id_RSAES_OAEP, "RSA/None/OAEPWithSHA-512AndMGF1Padding");
+	                	else if (algorithm.equals(OIWObjectIdentifiers.idSHA1.getId()))
+	                		retVal.setAlgorithmMapping(PKCSObjectIdentifiers.id_RSAES_OAEP, "RSA/None/OAEPWithSHA-1AndMGF1Padding");
+	                }
+	                else {
+	                	retVal.setAlgorithmMapping(PKCSObjectIdentifiers.id_RSAES_OAEP, "RSA/None/OAEPWithSHA1AndMGF1Paddingg");
+	                }					
+				}
+        	}
+        	else {
+
+                if( rsaesoaePparams != null) {
+                	String algorithm = rsaesoaePparams.getHashAlgorithm().getAlgorithm().getId();
+                	if (algorithm.equals(NISTObjectIdentifiers.id_sha256.getId()))
+                		retVal.setAlgorithmMapping(PKCSObjectIdentifiers.id_RSAES_OAEP, "RSA/ECB/OAEPWithSHA-256AndMGF1Padding");
+                	else if (algorithm.equals(NISTObjectIdentifiers.id_sha384.getId()))
+                		retVal.setAlgorithmMapping(PKCSObjectIdentifiers.id_RSAES_OAEP, "RSA/ECB/OAEPWithSHA-384AndMGF1Padding");
+                	else if (algorithm.equals(NISTObjectIdentifiers.id_sha512.getId()))
+                		retVal.setAlgorithmMapping(PKCSObjectIdentifiers.id_RSAES_OAEP, "RSA/ECB/OAEPWithSHA-512AndMGF1Padding");
+                	else if (algorithm.equals(OIWObjectIdentifiers.idSHA1.getId()))
+                		retVal.setAlgorithmMapping(PKCSObjectIdentifiers.id_RSAES_OAEP, "RSA/ECB/OAEPWithSHA-1AndMGF1Padding");
+                }
+                else {
+                	retVal.setAlgorithmMapping(PKCSObjectIdentifiers.id_RSAES_OAEP, "RSA/ECB/OAEPWithSHA-1AndMGF1Padding");
+                }
+        	}
+
     	}
     	
-        return retVal;
+    	return retVal;
     }
     
     public JceKTSKeyUnwrapper createAsymmetricUnwrapper(AlgorithmIdentifier keyEncryptionAlgorithm, PrivateKey keyEncryptionKey, byte[] partyUInfo, byte[] partyVInfo)
